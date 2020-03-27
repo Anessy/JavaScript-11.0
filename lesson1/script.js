5c308ead47a65e31305b17c56a7b530308b46df5
Anessy <n.repekha@gmail.com>
2020-03-27 15:21:35 +0200

    lesson2

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,3 +1,22 @@
 let money, income, addExpenses, deposit, mission, period;
-alert ('Hello world');
-console.log('Hello world');
+
+money = 12000; 
+income = 'freelance'; 
+addExpenses = 'Internet, Taxi, Cafe';
+deposit = true; 
+mission = 50000; 
+period = 12; 
+console.log('period: ', period);
+
+console.log(typeof money, typeof income, typeof addExpenses, typeof deposit, typeof mission, typeof period);
+console.log(addExpenses.length);
+console.log('Период равен ', period, ' месяцев');
+console.log('Цель заработать', mission, 'гривен');
+
+addExpenses = addExpenses.toLowerCase();
+addExpenses = addExpenses.split(', ');
+console.log(addExpenses);
+
+let budgetDay = money/30;
+console.log('budgetDay: ', budgetDay);
+//    - Вывести в консоль budgetDay
